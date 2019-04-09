c9ae15fbc0c53e6a52ad7e29b7c6fe010d1b3f76
Antoine <agiordan@z4r1p6.le-101.fr>
2019-04-09 16:39:09 +0200

    De-leaks itoa + create lltoa

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,12 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/07 17:09:15 by agiordan     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/09 16:32:38 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/09 16:37:53 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../include/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,6 +19,7 @@ int	main(int ac, char **av)
 {
 	av = NULL;
 	ac = 0;
-	printf("%d\n", ft_bintodec(110010));
+	printf("%s\n", ft_itoa(123456789));
+	printf("%s\n", ft_lltoa(1234567891011121314));
 	return (0);
 }
